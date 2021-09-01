8dc62ad6d3ed6544aa29c9ce69ac6f1fc9b5fbb8
emmazy <emmazy100@gmail.com>
2021-09-01 18:42:17 +0100

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const emailjs = require('emailjs-com')
 
 const submitForm = (e) => {
     e.preventDefault();
-    emailjs.sendForm('suraj_ojha', 'my_template', 'contact-form', 'user_EG4VqNgwLQYFWhLe81dxx')
+    emailjs.sendForm('service_t5x7lp9', 'template_gl7riep', 'contact-form', 'user_Vbeqkopgu0Wj8bJJ1cRUS')
      .then(result => alert('Your message has been sent. I will be in contact with you soon.Thank you!'))
      .catch(err => alert('Oops! Please try again.'))
 
